c855aca1a9da63c7575de9ec2d6808f2fcf599a0
stanly888 <a0918853297@icloud.com>
2025-04-03 00:57:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print('SmartTrader Main Running')
+# main.py
+from src.multi_trainer import MultiStrategyTrainer
+from src.utils import fetch_real_data, load_config
+from src.logger import log_strategy_summary
+
+if __name__ == "__main__":
+    print("SmartTrader Main Running")
+
+    config = load_config()
+    close, high, low, volume = fetch_real_data(config)
+
+    trainer = MultiStrategyTrainer(config)
+    trainer.train_all(close, high, low, volume)
