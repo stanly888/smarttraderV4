6fa5adb85cbdbcc5b0965044eca6116ea579d076
stanly888 <a0918853297@icloud.com>
2025-04-12 14:27:57 +0800

    Delete loop.py

--- loop.py
+++
@@ -1,9 +0,0 @@
-
-import time
-from trainer import train_model
-from telegram import send_strategy_update
-
-while True:
-    result = train_model()
-    send_strategy_update(result)
-    time.sleep(900)  # 每 15 分鐘 retrain 一次
