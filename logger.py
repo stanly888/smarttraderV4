6e1e16e6bd462c648e31a504861a75b10f0c16ef
stanly888 <a0918853297@icloud.com>
2025-04-18 23:15:58 +0800

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,5 +1,10 @@
 import json
 
 def record_retrain_status(model_name, reward, confidence):
+    data = {
+        "model": model_name,
+        "score": float(reward),
+        "confidence": float(confidence)
+    }
     with open("retrain_status.json", "w") as f:
-        json.dump({"model": model_name, "score": reward, "confidence": confidence}, f)
+        json.dump(data, f, indent=2)
